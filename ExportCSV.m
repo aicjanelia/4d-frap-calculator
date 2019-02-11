368f58d0bcf50363a162b5745f8606948904d58e
Eric Wait <eric@waitphoto.com>
2019-02-11 09:57:22 -0500

    Created a CSV export function

---
+++ ExportCSV.m
@@ -0,0 +1,33 @@
+function ExportCSV(src,event)
+    global REC_HANDLES IM IM_METADATA FLOUR_MEAN_PER_T
+    
+    [fName,fPath] = uiputfile('*.csv','Save file as...');
+
+    recPosZ_xy = get(REC_HANDLES(1),'Position');
+    recPosX_zy = get(REC_HANDLES(2),'Position');
+    
+    yRad = recPosZ_xy(4)/2;
+    xRad = recPosZ_xy(3)/2;
+    zRad = recPosX_zy(3)/2;
+    
+    yCenter = recPosZ_xy(2) + yRad;
+    xCenter = recPosZ_xy(1) + xRad;
+    zCenter = recPosX_zy(1) + zRad;
+
+    [frapCorrected, frapMean,fitMetrics] = CalcFrapCurves([yCenter,xCenter,zCenter],[yRad,xRad,zRad],IM,IM_METADATA,FLOUR_MEAN_PER_T,IM_METADATA.FrapChannel);
+    
+    fHandle = fopen(fullfile(fPath,fName),'wt');
+    fprintf(fHandle,'Image Name,%s\n',IM_METADATA.DatasetName);
+    fprintf(fHandle,'\n');
+    
+    fprintf(fHandle,'A,+/-,t1/2,+/-,R-Squared\n');
+    fprintf(fHandle,'%f,%f,%f,%f,%f\n',fitMetrics.A,fitMetrics.A_confidance,fitMetrics.thalf,fitMetrics.thalf_confidance,fitMetrics.Rsquared);
+    fprintf(fHandle,'\n');
+    
+    fprintf(fHandle,'Time (s), FRAP (original), FRAP (correct)\n');
+    for t=1:IM_METADATA.NumberOfFrames
+        fprintf(fHandle,'%f,%f,%f\n',IM_METADATA.TimeStampDelta(t),frapMean(t),frapCorrected(t));
+    end
+    
+    fclose(fHandle);
+end
