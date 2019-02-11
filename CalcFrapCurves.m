6d9241a3890630111bfff0f4f4341c7fbfcafd3e
Eric Wait <eric@waitphoto.com>
2019-02-11 09:56:59 -0500

    Put curve fitting metrics in FRAP Curves function

--- CalcFrapCurves.m
+++ CalcFrapCurves.m
@@ -1,4 +1,4 @@
-function [frapCorrected, frapMean] = CalcFrapCurves(center_rcz,radius_rcz,im,imMetadata,flourMean,channel)   
+function [frapCorrected, frapMean, fitMetrics] = CalcFrapCurves(center_rcz,radius_rcz,im,imMetadata,flourMean,channel)   
     maskInd = GetPixelIndList(center_rcz,radius_rcz,imMetadata.Dimensions([2,1,3]));
     
     frapMean = zeros(imMetadata.NumberOfFrames,1);
@@ -10,4 +10,24 @@ function [frapCorrected, frapMean] = CalcFrapCurves(center_rcz,radius_rcz,im,imM
     
     frapMean = ImUtils.ConvertType(frapMean,'double',true);
     frapCorrected = frapMean./flourMean;
+    
+    frapCorrected = frapCorrected./max(frapCorrected);% TODO: change this when we have a starting frame that is past the first frame
+    
+    ft = fittype('A*(1-exp(-tau*t))','independent','t','coefficients',{'A','tau'});
+    [fm,gof] = fit(imMetadata.TimeStampDelta',frapCorrected,ft,'Startpoint',[0.5,0.05],'Lower',[0 0],'Upper',[inf inf]);
+
+    coeffs = coeffvalues(fm);
+    confints = confint(fm);
+    A1conf = diff(confints(:,1))/2;
+    thalf1 = log(2)/coeffs(2);
+    thalf1confl = log(2)/confints(2,2);
+    thalf1confu = log(2)/confints(1,2);
+    thalf1conf = abs(thalf1confu - thalf1confl)/2;
+    
+    fitMetrics = struct('A',[],'A_confidance',[],'thalf',[],'thalf_confidance',[],'Rsquared',[]);
+    fitMetrics.A = coeffs(1);
+    fitMetrics.A_confidance = A1conf;
+    fitMetrics.thalf = thalf1;
+    fitMetrics.thalf_confidance = thalf1conf;
+    fitMetrics.Rsquared = gof.adjrsquare;
 end
--- UpdateFRAPplot.m
+++ UpdateFRAPplot.m
@@ -1,5 +1,5 @@
 function UpdateFRAPplot()
-    global REC_HANDLES AXES_HANDLES IM IM_METADATA FLOUR_MEAN_PER_T
+    global REC_HANDLES AXES_HANDLES IM IM_METADATA FLOUR_MEAN_PER_T TEXT_HANDLES
 
     recPosZ_xy = get(REC_HANDLES(1),'Position');
     recPosX_zy = get(REC_HANDLES(2),'Position');
@@ -12,11 +12,16 @@ function UpdateFRAPplot()
     xCenter = recPosZ_xy(1) + xRad;
     zCenter = recPosX_zy(1) + zRad;
 
-    [frapCorrected, frapMean] = CalcFrapCurves([yCenter,xCenter,zCenter],[yRad,xRad,zRad],IM,IM_METADATA,FLOUR_MEAN_PER_T,IM_METADATA.FrapChannel);
+    [frapCorrected, frapMean, fitMetrics] = CalcFrapCurves([yCenter,xCenter,zCenter],[yRad,xRad,zRad],IM,IM_METADATA,FLOUR_MEAN_PER_T,IM_METADATA.FrapChannel);
     
     hold(AXES_HANDLES(4),'off');
     plot(IM_METADATA.TimeStampDelta,frapMean,'o','DisplayName','Raw FRAP data','parent',AXES_HANDLES(4));
     hold(AXES_HANDLES(4),'on');
     plot(IM_METADATA.TimeStampDelta,frapCorrected,'o','DisplayName','Corrected FRAP data','parent',AXES_HANDLES(4));
     legend(AXES_HANDLES(4),'Location','southeast')
+    title(AXES_HANDLES(4),'Frap Data');
+    xlabel(AXES_HANDLES(4),'Time (s)')
+    ylabel(AXES_HANDLES(4),'Intensity (au)')
+    
+    set(TEXT_HANDLES(1),'string',sprintf('A=%.02f (+/-%.02f)  t1/2=%.02f(+/-%.02f)  R-squared:%.02f',fitMetrics.A,fitMetrics.A_confidance,fitMetrics.thalf,fitMetrics.thalf_confidance,fitMetrics.Rsquared));
 end
